60abc1648bafcc7689c213569e51e34ac94d6ea2
lirenxiang <lrx0014@hotmail.com>
2022-06-09 18:24:20 +0800

    Initial Commit

---
+++ Shared/whoisApp.swift
@@ -0,0 +1,17 @@
+//
+//  whoisApp.swift
+//  Shared
+//
+//  Created by Li Renxiang on 2022/6/9.
+//
+
+import SwiftUI
+
+@main
+struct whoisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
